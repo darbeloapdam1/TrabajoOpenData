0838fa68c3bfb65e077dea7eb6a8b5ef5b31eb4d
darbeloapdam1 <darbeloapdam1@gmail.com>
2022-02-22 12:37:51 +0100

    empezar proyecto

---
+++ TrabajoOpenData/TrabajoOpenData/Models/Municipio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrabajoOpenData.Models
+{
+    public class Municipio
+    {
+        string nombre { get; set; }
+        int nuevosCasos { get; set; }
+        int casosAcumulados { get; set; }
+
+        public Municipio(string nombre, int nuevosCasos, int casosAcumulados)
+        {
+            this.nombre = nombre;
+            this.nuevosCasos = nuevosCasos;
+            this.casosAcumulados = casosAcumulados;
+        }
+    }
+}
---
+++ TrabajoOpenData/TrabajoOpenData/Pages/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+
+namespace TrabajoOpenData
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            cargarDatos();
+        }
+
+        private void cargarDatos()
+        {
+            if (internetConnectionCheck())
+            {
+
+            }
+            else
+            {
+                bool respuesta = DisplayAlert("No tienes acceso a internet", "¿Quieres intentar conectarte?", "Si", "No").Result;
+                if (respuesta)
+                {
+                    var profiles = Connectivity.ConnectionProfiles;
+                    if (profiles.Contains(ConnectionProfile.WiFi))
+                    {
+                        
+                    }
+                    else
+                    {
+                        DisplayAlert("Error de conexión", "No se puede conectar a internet", "Aceptar");
+                    }
+                }
+            }
+        }
+
+        private bool internetConnectionCheck()
+        {
+            var current = Connectivity.NetworkAccess;
+            return current == NetworkAccess.Internet;           
+        }
+    }
+}
